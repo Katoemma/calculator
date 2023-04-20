b5c112ea971d23339ee76ed69a7216144c56f637
kato emma <emmanuelkato39@gmail.com>
2023-04-20 18:07:48 +0300

    designed the interface

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,29 @@
 module.exports = {
   content: ["./public/*.html"],
   theme: {
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+    colors:{
+      "white": "#ffffff",
+      "displaygray": "#b9b992",
+      "black": "#000000",
+      "reddish": "#ff1a1a"
+    },
     extend: {},
   },
   plugins: [],
 }
-
